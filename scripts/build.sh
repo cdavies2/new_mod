190eb489d19d3380e51803ce8ca9c38d520940eb
Cassandra Davies <ac.cdavies@dev1.chicago.kbase.us>
2024-07-19 16:09:26 +0000

    Fixed install bug

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@ cd /kb/module/report-app
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 chmod +x /root/.nvm/nvm.sh
 . /root/.nvm/nvm.sh &&  nvm install 20
-npm install --global typescript
-npm run install
+#npm install --global typescript
+npm install
 npm run build 
