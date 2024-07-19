a1a98a490bea8dda57ab75d3108779556754f5f1
Cassandra Davies <ac.cdavies@dev1.chicago.kbase.us>
2024-07-19 15:47:57 +0000

    Modified dockerignore

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ cd /kb/module/report-app
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 chmod +x /root/.nvm/nvm.sh
 . /root/.nvm/nvm.sh &&  nvm install 20
-npm install --global typescript
+#npm install --global typescript
 npm run install
 npm run build 
 # --output-path /kb/module/report-app/dist
